551ad16a182f6582199ac77d034652e068c91511
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2024-08-04 17:04:23 +0530

    complete

--- mail-receiver.js
+++ mail-receiver.js
@@ -2,25 +2,22 @@ const SMTPServer = require("smtp-server").SMTPServer;
 const fs = require('fs');
 
 const server = new SMTPServer({
-    secure: false, // Set to true for TLS support
-    authOptional: true, // Disable authentication for simplicity
+    secure: false,
+    authOptional: true,
     onMailFrom: (mail, session, callback) => {
         console.log(mail);
         callback(null);
     },
     onData: (stream, session, callback) => {
         let emailData = '';
-
         stream.on('data', (chunk) => {
             emailData += chunk;
         });
-
         stream.on('end', () => {
             fs.writeFileSync('email.eml', emailData); // Save email to a file
             callback(null, 'Message accepted'); // Indicate that the message was accepted
         });
     },
-
     onAuth(auth, session, callback) {
         callback(null, { user: auth.username }); // Allow any username/password for simplicity
     },
